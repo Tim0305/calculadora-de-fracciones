bdc5e0ba80f09d03232b32a24dca43482b3fca8f
Timoteo <tabtim1015@gmail.com>
2024-09-06 16:52:24 -0600

    Feat: Se agrego la capacidad de mandar errores al main

--- src/conversor/ConversorFracciones.java
+++ src/conversor/ConversorFracciones.java
@@ -4,28 +4,37 @@ import fraccion.Fraccion;
 
 public class ConversorFracciones {
 
+    private static String denominadorString;
+
     public static void toFraccion(String fraccionString) {
         // Numerador
         int numerador = getNumerador(fraccionString.trim());
         System.out.println("Numerador: " + numerador);
+        System.out.println(denominadorString);
     }
 
+    /*
+    Metodo para obtener el numerador en entero y al mismo tiempo el denominador en string
+    Cuando se ejecuta correctamente el metodo el denominador se almacena en la variable denominadorString
+     */
     private static int getNumerador(String fraccion) {
+
         String[] componentes = fraccion.split(" ");
 
         // Digito
         if (componentes[0].equalsIgnoreCase("uno")) {
             throw new Error("El numerador 'uno' no es un numerador valido");
         } else if (componentes[0].equalsIgnoreCase("un")) {
+            denominadorString = buildStringFromIndex(componentes, 1);
             return 1;
         } else if (DiccionarioNumerador.existsDigito(componentes[0])) {
-            int numerador = DiccionarioNumerador.getDigito(componentes[0]);
-
-            return numerador;
+            denominadorString = buildStringFromIndex(componentes, 1);
+            return DiccionarioNumerador.getDigito(componentes[0]);
         }
 
         // Especiales
         if (DiccionarioNumerador.existsEspecial(componentes[0])) {
+            denominadorString = buildStringFromIndex(componentes, 1);
             return DiccionarioNumerador.getEspecial(componentes[0]);
         }
 
@@ -35,10 +44,15 @@ public class ConversorFracciones {
 
             String digito = componentes[0].replace("veinti", "");
 
-            if (DiccionarioNumerador.existsDigito(digito)) {
+            if (digito.equalsIgnoreCase("uno")) {
+                throw new RuntimeException("El numerador 'veintiuno' no es un numerador valido");
+            } else if (digito.equalsIgnoreCase("un")) {
+                numerador++;
+            } else if (DiccionarioNumerador.existsDigito(digito)) {
                 numerador += DiccionarioNumerador.getDigito(digito);
             }
 
+            denominadorString = buildStringFromIndex(componentes, 1);
             return numerador;
         }
 
@@ -51,12 +65,32 @@ public class ConversorFracciones {
                 if (componentes[1].equalsIgnoreCase("y")) {
                     if (DiccionarioNumerador.existsDigito(componentes[2])) {
                         numerador += DiccionarioNumerador.getDigito(componentes[2]);
+                        denominadorString = buildStringFromIndex(componentes, 3);
                     }
+                } else {
+                    denominadorString = buildStringFromIndex(componentes, 1);
                 }
             }
 
             return numerador;
         }
-        return -1;
+
+        throw new RuntimeException("El numerador no es valido'");
+    }
+
+    /*
+    Este metodo recibe un arreglo de strings y un indice a partir del cual se va a construir un nuevo string
+    Ejemplo: si el indice es 1 se construira un string a partir del indice 1 del arreglo de strings hasta el ultimo elemento del array
+     */
+    private static String buildStringFromIndex(String[] arrayStrings, int index) {
+
+        StringBuilder completeString = new StringBuilder();
+
+        for (int i = index, l = arrayStrings.length; i < l; i++) {
+            completeString.append(arrayStrings[i]);
+            completeString.append(" ");
+        }
+
+        return completeString.toString();
     }
 }
